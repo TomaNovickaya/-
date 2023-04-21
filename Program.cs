3d5a7f9362f549512c9bb049a0bed09a43b1aed5
Tamara <novickaya.toma@mail.ru>
2023-04-21 21:55:53 +0300

    Добавила код программы

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class StringArrays {
+	static void Main() {
+		string[] arr1 = new string [4]  { "Hello", "2", "world", ":-)" };
+        string[] arr2 = new string [arr1.Length];
+       
+        Console.WriteLine();
+		Console.WriteLine("Исходный массив: ");
+		for(int i=0; i < arr1.Length; i++)
+			Console.Write(arr1[i] + " ");
+		Console.WriteLine("\n");
+		
+		Console.WriteLine("Новый массив массив: ");
+        
+		for(int i=0; i < arr1.Length; i++)
+      {
+        if (arr1[i].Length<=3) 
+       {
+        arr2[i]=arr1[i];
+        
+       }
+        Console.Write(arr2[i] + " ");
+      }
+		
+	}
+}
